8844e1db229641206cf7787b71ade65312dad568
Кирилин Юрий Александрович <kirilin.ya@avgd.pro>
2022-08-22 15:49:54 +0400

    first commit

---
+++ app/src/main/java/com/plcoding/weatherapp/data/location/DefaultLocationTracker.kt
@@ -0,0 +1,4 @@
+package com.plcoding.weatherapp.data.location
+
+class DefaultLocationTracker {
+}
---
+++ app/src/main/java/com/plcoding/weatherapp/domain/weather/WeatherData.kt
@@ -0,0 +1,11 @@
+package com.plcoding.weatherapp.domain.weather
+
+import java.time.LocalDateTime
+
+data class WeatherData(
+    val time: LocalDateTime,
+    val temperatureCelcius: Double,
+    val pressure: Double,
+    val humidity: Double,
+    val weatherType: WeatherType
+)
